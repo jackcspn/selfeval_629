e7837ed72e6fa403014a957ff0e6b5efe049ae72
EC2 Default User <ec2-user@ip-172-31-33-46.us-east-2.compute.internal>
2019-03-28 18:44:57 +0000

    Change Question Interface

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,7 @@ class QuestionsController < ApplicationController
     # puts("user_id")
     # puts(@user_id)
     # @questions = Question.find{|question| question.uid == @user_id || question.uid == nil}
-    if @user_id == 1
+    if current_user.admin?
       @questions = Question.all
     else
       @questions = Question.where(uid: @user_id)
@@ -52,7 +52,8 @@ class QuestionsController < ApplicationController
       @question.answer = params[:question][:answer]
       @question.explanation = params[:question][:explanation]
       @question.uid = current_user.id
-      # @question.display = false
+      # @question.approved = 'false'
+      @question.display = false
       @question.feedback = nil
      
       #if @question.answer == "True" or @question.answer == "true"
@@ -66,8 +67,9 @@ class QuestionsController < ApplicationController
       @question.uid = current_user.id
       # @question.display = false
       @question.feedback = nil
+      @question.approved = false
     end
-    if current_user.id == 1
+    if current_user.admin?
        @question.display = true
     else
       @question.display = false
@@ -75,7 +77,7 @@ class QuestionsController < ApplicationController
     
     respond_to do |format|
       if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
+        format.html { redirect_to @question, notice: 'Question was successfully created, waiting for approving' }
         format.json { render :show, status: :created, location: @question }
       else
         format.html { render :new }
